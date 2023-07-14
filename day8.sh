266fb226976b421b2a2568dbe4d1163e0bcebfa7
root <root@ip-172-31-38-132.ap-south-1.compute.internal>
2023-07-14 18:13:27 +0000

    task8

---
+++ day8.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Below loop executes the command between do and done based on the result
+while <condition>
+do
+    <command 1>
+    <command 2>
+done
+
+#Below loop executes the command between do and done for items in the list
+for <var> in <value1 value2 ... valuen>
+do
+    <command 1>
+    <command 2>
+    <etc>
+done
+
+#Below loop executes the command between do and done until the condition is true
+until <condition>
+do
+    <command 1>
+    <command 2>
+    <etc>
+done
